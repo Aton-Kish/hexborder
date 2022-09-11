af44a992e8d74249347036500d316e28eabeb4fe
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-11 23:55:25 +0900

    integ modmenu

---
+++ src/main/java/atonkish/hexborder/HexBorderConfig.java
@@ -0,0 +1,8 @@
+package atonkish.hexborder;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = HexBorderMod.MOD_ID)
+public class HexBorderConfig implements ConfigData {
+}
--- src/main/java/atonkish/hexborder/HexBorderMod.java
+++ src/main/java/atonkish/hexborder/HexBorderMod.java
@@ -3,13 +3,20 @@ package atonkish.hexborder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+
 import net.fabricmc.api.ModInitializer;
 
 public class HexBorderMod implements ModInitializer {
 	public static final String MOD_ID = "reputation";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static HexBorderConfig CONFIG;
 
 	@Override
 	public void onInitialize() {
+		// Auto Config
+		AutoConfig.register(HexBorderConfig.class, GsonConfigSerializer::new);
+		CONFIG = AutoConfig.getConfigHolder(HexBorderConfig.class).getConfig();
 	}
 }
