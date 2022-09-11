54be13b71353c5c0802429c949b6826d564a6bd9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-11 23:16:17 +0900

    init mod

---
+++ src/main/java/atonkish/hexborder/HexBorderMod.java
@@ -0,0 +1,15 @@
+package atonkish.hexborder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HexBorderMod implements ModInitializer {
+	public static final String MOD_ID = "reputation";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
