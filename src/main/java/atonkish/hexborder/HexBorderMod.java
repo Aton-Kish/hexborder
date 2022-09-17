4e4259865d8e157211d995b04ac66b1446dc0b6c
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-17 23:58:39 +0900

    update autoconfig entrypoint

--- src/main/java/atonkish/hexborder/HexBorderMod.java
+++ src/main/java/atonkish/hexborder/HexBorderMod.java
@@ -3,13 +3,19 @@ package atonkish.hexborder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.GsonBuilder;
+
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigHolder;
+import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import me.shedaniel.clothconfig2.api.ModifierKeyCode;
 
 import net.fabricmc.api.ModInitializer;
 
 import atonkish.hexborder.integration.autoconfig.ModSave;
+import atonkish.hexborder.integration.autoconfig.ModifierKeyCodeGsonAdapter;
+import atonkish.hexborder.integration.autoconfig.ModifierKeyCodeGuiProvider;
 
 public class HexBorderMod implements ModInitializer {
 	public static final String MOD_ID = "hexborder";
@@ -19,7 +25,15 @@ public class HexBorderMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// Auto Config
-		CONFIG_MANAGER = AutoConfig.register(HexBorderConfig.class, GsonConfigSerializer::new);
+		GuiRegistry registry = AutoConfig.getGuiRegistry(HexBorderConfig.class);
+		registry.registerPredicateProvider(new ModifierKeyCodeGuiProvider(),
+				field -> field.getType() == ModifierKeyCode.class);
+
+		CONFIG_MANAGER = AutoConfig.register(HexBorderConfig.class,
+				(definition, configClass) -> new GsonConfigSerializer<>(definition, configClass, new GsonBuilder()
+						.registerTypeAdapter(ModifierKeyCode.class, new ModifierKeyCodeGsonAdapter.Serializer())
+						.registerTypeAdapter(ModifierKeyCode.class, new ModifierKeyCodeGsonAdapter.Deserializer())
+						.create()));
 		CONFIG_MANAGER.registerSaveListener(new ModSave<HexBorderConfig>());
 	}
 }
