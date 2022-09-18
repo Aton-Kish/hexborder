0604e2ae29d5300d02cc7630cc4ed4f5eeae7683
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-18 12:44:33 +0900

    update config

--- src/main/java/atonkish/hexborder/HexBorderConfig.java
+++ src/main/java/atonkish/hexborder/HexBorderConfig.java
@@ -1,9 +1,28 @@
 package atonkish.hexborder;
 
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.clothconfig2.api.Modifier;
+import me.shedaniel.clothconfig2.api.ModifierKeyCode;
+
+import net.minecraft.client.util.InputUtil.Type;
+
+import org.lwjgl.glfw.GLFW;
 
 import atonkish.hexborder.integration.autoconfig.ModConfigData;
+import atonkish.hexborder.integration.autoconfig.annotation.ModConfigEntry;
 
 @Config(name = HexBorderMod.MOD_ID)
 public class HexBorderConfig implements ModConfigData {
+    @ModConfigEntry.HexagonSide(min = 4)
+    public int side = 8;
+
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+    public F3KeyBindings f3KeyBindings = new F3KeyBindings();
+
+    public static class F3KeyBindings {
+        public ModifierKeyCode toggleShowBorderKey = ModifierKeyCode.of(
+                Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_X),
+                Modifier.none());
+    }
 }
--- src/main/java/atonkish/hexborder/mixin/KeyboardMixin.java
+++ src/main/java/atonkish/hexborder/mixin/KeyboardMixin.java
@@ -1,7 +1,5 @@
 package atonkish.hexborder.mixin;
 
-import org.lwjgl.glfw.GLFW;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,8 +9,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil.Key;
 import net.minecraft.text.Text;
 
+import atonkish.hexborder.HexBorderConfig;
+import atonkish.hexborder.HexBorderMod;
 import atonkish.hexborder.client.render.debug.DebugRendererAccessor;
 
 @Mixin(Keyboard.class)
@@ -29,7 +30,9 @@ public abstract class KeyboardMixin {
 
     @Inject(method = "processF3", at = @At("HEAD"), cancellable = true)
     public void processF3(final int key, final CallbackInfoReturnable<Boolean> cir) {
-        if (key == GLFW.GLFW_KEY_X) {
+        HexBorderConfig config = HexBorderMod.CONFIG_MANAGER.get();
+        Key keyCode = config.f3KeyBindings.toggleShowBorderKey.getKeyCode();
+        if (key == keyCode.getCode()) {
             boolean bl2 = ((DebugRendererAccessor) this.client.debugRenderer).toggleShowHexBorder();
             this.debugLog(bl2 ? "debug.hexagon_boundaries.on" : "debug.hexagon_boundaries.off", new Object[0]);
             cir.setReturnValue(true);
