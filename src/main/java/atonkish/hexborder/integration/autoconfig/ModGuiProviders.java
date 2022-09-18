44ea400edc356f4801dc82308173b31e8cf3b9d7
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-09-18 11:43:14 +0900

    add annotation

--- src/main/java/atonkish/hexborder/integration/autoconfig/ModGuiProviders.java
+++ src/main/java/atonkish/hexborder/integration/autoconfig/ModGuiProviders.java
@@ -1,13 +1,18 @@
 package atonkish.hexborder.integration.autoconfig;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
 import me.shedaniel.autoconfig.util.Utils;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.api.ModifierKeyCode;
+
 import net.minecraft.text.Text;
+
+import atonkish.hexborder.HexBorderMod;
+import atonkish.hexborder.integration.autoconfig.annotation.ModConfigEntry;
 
 public class ModGuiProviders {
     private static final ConfigEntryBuilder ENTRY_BUILDER = ConfigEntryBuilder.create();
@@ -26,6 +31,58 @@ public class ModGuiProviders {
         registry.registerAnnotationProvider(
                 (i18n, field, config, defaults, guiProvider) -> Collections.emptyList(),
                 ConfigEntry.Gui.Excluded.class);
+
+        registry.registerAnnotationProvider(
+                (i18n, field, config, defaults, guiProvider) -> {
+                    ModConfigEntry.HexagonSide side = field.getAnnotation(ModConfigEntry.HexagonSide.class);
+
+                    return Collections.singletonList(
+                            ENTRY_BUILDER
+                                    .startIntField(Text.translatable(i18n),
+                                            Utils.getUnsafely(field, config, (int) side.min()))
+                                    .setDefaultValue(() -> Utils.getUnsafely(field, defaults))
+                                    .setSaveConsumer(newValue -> Utils.setUnsafely(field, config, newValue))
+                                    .setMin((int) side.min())
+                                    .setErrorSupplier((value) -> {
+                                        if (value % 2 != 0) {
+                                            return Optional
+                                                    .of(Text.translatable("text.cloth-config."
+                                                            + HexBorderMod.MOD_ID
+                                                            + ".error.odd"));
+                                        }
+
+                                        return Optional.empty();
+                                    })
+                                    .build());
+                },
+                field -> field.getType() == int.class || field.getType() == Integer.class,
+                ModConfigEntry.HexagonSide.class);
+
+        registry.registerAnnotationProvider(
+                (i18n, field, config, defaults, guiProvider) -> {
+                    ModConfigEntry.HexagonSide side = field.getAnnotation(ModConfigEntry.HexagonSide.class);
+
+                    return Collections.singletonList(
+                            ENTRY_BUILDER
+                                    .startLongField(Text.translatable(i18n),
+                                            Utils.getUnsafely(field, config, side.min()))
+                                    .setDefaultValue(() -> Utils.getUnsafely(field, defaults))
+                                    .setSaveConsumer(newValue -> Utils.setUnsafely(field, config, newValue))
+                                    .setMin(side.min())
+                                    .setErrorSupplier((value) -> {
+                                        if (value % 2 != 0) {
+                                            return Optional
+                                                    .of(Text.translatable("text.cloth-config."
+                                                            + HexBorderMod.MOD_ID
+                                                            + ".error.odd"));
+                                        }
+
+                                        return Optional.empty();
+                                    })
+                                    .build());
+                },
+                field -> field.getType() == long.class || field.getType() == Long.class,
+                ModConfigEntry.HexagonSide.class);
 
         return registry;
     }
